5446e148f6503aff716c41633e3200d8ebf6d1c2
Lucas <lucasvanlume@hotmail.com>
2023-09-04 18:44:40 -0300

    feat(community): Added the basic structure of the feed layout and Tab Bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,55 +1,190 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { View, Text, Platform, StyleSheet } from 'react-native'
+import React from 'react'
+import {
+    Feather,
+    Ionicons,
+    MaterialCommunityIcons,
+    Entypo,
+    FontAwesome,
+    FontAwesome5,
+} from '@expo/vector-icons'
 import { Link, Tabs } from 'expo-router';
-import { Pressable, useColorScheme } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { COLORS } from '../../constants'
+import { LinearGradient } from 'expo-linear-gradient'
 
-import Colors from '../../constants/Colors';
+const Tab = createBottomTabNavigator()
 
-/**
- * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
- */
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
+const styles = StyleSheet.create({
+    tabBarStyle: {
+        position: 'absolute',
+        bottom: 0,
+        right: 0,
+        left: 0,
+        elevation: 0,
+        height: 60,
+        backgroundColor: COLORS.primary,
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+    },
+});
+
+const screenOptions = {
+    tabBarShowLabel: false,
+    headerShown: false,
+    tabBarHideOnKeyboard: true,
+    tabBarStyle: styles.tabBarStyle
+};
+
+const BottomTabNavigation = () => {
+    return (
+        <Tabs screenOptions={screenOptions}>
+            <Tabs.Screen
+                name="index"
+                options={{
+                    tabBarIcon: ({ focused }) => {
+                        return (
+                            <FontAwesome5
+                                name="users"
+                                size={24}
+                                color={focused ? COLORS.white : COLORS.tabSecondary}
+                            />
+                        )
+                    },
+                }}
+            />
+            <Tabs.Screen
+                name="rankingTest"
+                options={{
+                    tabBarIcon: ({ focused }) => {
+                        return (
+                            <Entypo
+                                name="bar-graph"
+                                size={24}
+                                color={focused ? COLORS.white : COLORS.tabSecondary}
+                            />
+                        )
+                    },
+                }}
+            />
+
+            <Tabs.Screen
+                name="two"
+                options={{
+                    tabBarIcon: ({ focused }) => {
+                        return (
+                            <LinearGradient
+                                colors={['#4B0082', '#6D458B']}
+                                style={{
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                    width: Platform.OS == 'ios' ? 50 : 60,
+                                    height: Platform.OS == 'ios' ? 50 : 60,
+                                    top: Platform.OS == 'ios' ? -10 : -20,
+                                    borderRadius: 22,
+                                }}
+                            >
+                                <Feather
+                                    name="plus-circle"
+                                    size={24}
+                                    color={COLORS.white}
+                                />
+                            </LinearGradient>
+                        )
+                    },
+                }}
+            />
+
+            <Tabs.Screen
+                name="trainingTest"
+                options={{
+                    tabBarIcon: ({ focused }) => {
+                        return (
+                            <MaterialCommunityIcons
+                                name="weight-lifter"
+                                size={24}
+                                color={focused ? COLORS.white : COLORS.tabSecondary}
+                            />
+                        )
+                    },
+                }}
+            />
+
+            <Tabs.Screen
+                name="achievementsTest"
+                options={{
+                    tabBarIcon: ({ focused }) => {
+                        return (
+                            <MaterialCommunityIcons
+                                name="trophy"
+                                size={24}
+                                color={focused ? COLORS.white : COLORS.tabSecondary}
+                            />
+                        )
+                    },
+                }}
+            />
+        </Tabs>
+    )
 }
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
+export default BottomTabNavigation
 
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="two"
-        options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
-}
+
+
+
+// import FontAwesome from '@expo/vector-icons/FontAwesome5';
+// import { Link, Tabs } from 'expo-router';
+// import { Pressable, useColorScheme } from 'react-native';
+
+// import Colors from '../../constants/Colors';
+
+// /**
+//  * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
+//  */
+// function TabBarIcon(props: {
+//   name: React.ComponentProps<typeof FontAwesome>['name'];
+//   color: string;
+// }) {
+//   return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
+// }
+
+// export default function TabLayout() {
+//   const colorScheme = useColorScheme();
+
+//   return (
+//     <Tabs
+//       screenOptions={{
+//         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+//       }}>
+//       <Tabs.Screen
+//         name="index"
+//         options={{
+//           title: 'Tab One',
+//           tabBarIcon: ({ color }) => <TabBarIcon name="users" color={color} />,
+//           headerRight: () => (
+//             <Link href="/modal" asChild>
+//               <Pressable>
+//                 {({ pressed }) => (
+//                   <FontAwesome
+//                     name="info-circle"
+//                     size={25}
+//                     color={Colors[colorScheme ?? 'light'].text}
+//                     style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+//                   />
+//                 )}
+//               </Pressable>
+//             </Link>
+//           ),
+//         }}
+//       />
+//       <Tabs.Screen
+//         name="two"
+//         options={{
+//           title: 'Tab Two',
+//           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+//         }}
+//       />
+//     </Tabs>
+//   );
+// }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,300 @@
-import { StyleSheet } from 'react-native';
+import {
+    View,
+    Text,
+    TouchableOpacity,
+    ScrollView,
+    FlatList,
+    Image,
+    TextInput,
+} from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { COLORS, FONTS, SIZES, images } from '../../constants'
+import {
+    MaterialIcons,
+    Ionicons,
+    Feather,
+    Foundation,
+    MaterialCommunityIcons,
+} from '@expo/vector-icons'
+import { LinearGradient } from 'expo-linear-gradient'
+import { friends } from '../../constants/data'
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+const users = [images.user1, images.user2, images.user3, images.user4]
 
-export default function TabOneScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
-  );
+const Feed = () => {
+
+    function renderFeedPost() {
+        return (
+            <View
+                style={{
+                    backgroundColor: '#fff',
+                    flexDirection: 'column',
+                    width: '100%',
+                    borderRadius: 26,
+                    borderWidth: 1,
+                    borderColor: '#fff',
+                    marginVertical: 12,
+                }}
+            >
+                {/* Post header */}
+                <View
+                    style={{
+                        flexDirection: 'row',
+                        justifyContent: 'space-between',
+                        alignItems: 'center',
+                        marginTop: 8,
+                    }}
+                >
+                    <View
+                        style={{
+                            flexDirection: 'row',
+                            alignItems: 'center',
+                            marginHorizontal: 8,
+                        }}
+                    >
+                        <Image
+                            source={images.user1}
+                            style={{
+                                height: 52,
+                                width: 52,
+                                borderRadius: 20,
+                            }}
+                        />
+
+                        <View style={{ marginLeft: 12 }}>
+                            <Text
+                                style={{ ...FONTS.body4, fontWeight: 'bold' }}
+                            >
+                                Ankita Shrama
+                            </Text>
+                        </View>
+                    </View>
+
+                    <MaterialCommunityIcons
+                        name="dots-vertical"
+                        size={24}
+                        color={COLORS.black}
+                    />
+                </View>
+
+                {/* Post content */}
+
+                <View
+                    style={{
+                        marginHorizontal: 8,
+                        marginVertical: 8,
+                    }}
+                >
+                    <Text style={{ ...FONTS.body4 }}>
+                      djksajdklasjdkasjdkjskl djsakdjaskjdaksj dksjdkjas
+                      jdlaskjdklas jdkjasdkjak
+                    </Text>
+                </View>
+
+                <View
+                    style={{
+                        marginHorizontal: 8,
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Ionicons
+                        name="location-outline"
+                        size={21}
+                        color={COLORS.primary}
+                    />
+                    <Text
+                        style={{
+                            fontSize: 12,
+                            fontFamily: 'regular',
+                            color: COLORS.primary,
+                            marginLeft: 4,
+                        }}
+                    >
+                        Academia Couro e Osso | 10 mins atrás
+                    </Text>
+                </View>
+
+                {/* Posts likes and comments */}
+
+                <View
+                    style={{
+                        marginHorizontal: 8,
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        justifyContent: 'space-between',
+                        paddingBottom: 6,
+                    }}
+                >
+                    <View
+                        style={{
+                            flexDirection: 'row',
+                        }}
+                    >
+                        <View
+                            style={{
+                                flexDirection: 'row',
+
+                                alignItems: 'center',
+                                marginRight: SIZES.padding2,
+                            }}
+                        >
+                            <Feather
+                                name="heart"
+                                size={20}
+                                color={COLORS.black}
+                            />
+                            <Text style={{ ...FONTS.body4, marginLeft: 2 }}>
+                                22
+                            </Text>
+                        </View>
+
+                        <View
+                            style={{
+                                flexDirection: 'row',
+
+                                alignItems: 'center',
+                            }}
+                        >
+                            <MaterialCommunityIcons
+                                name="message-text-outline"
+                                size={20}
+                                color={COLORS.black}
+                            />
+                            <Text style={{ ...FONTS.body4, marginLeft: 2 }}>
+                                22
+                            </Text>
+                        </View>
+                    </View>
+
+                    <View style={{ flexDirection: 'row' }}>
+                        <View>
+                            <Text
+                                style={{ ...FONTS.body4, fontWeight: 'bold' }}
+                            >
+                                X
+                            </Text>
+                        </View>
+                        <View
+                            style={{
+                                flexDirection: 'row',
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                                marginLeft: 10,
+                            }}
+                        >
+                            {users.map((user, index) => (
+                                <Image
+                                    source={user}
+                                    key={index}
+                                    style={{
+                                        width: 25,
+                                        height: 25,
+                                        borderRadius: 999,
+                                        borderWidth: 1,
+                                        borderColor: '#fff',
+                                        marginLeft: -5,
+                                    }}
+                                />
+                            ))}
+                        </View>
+                    </View>
+                </View>
+
+                {/* comment section */}
+
+                <View
+                    style={{
+                        flexDirection: 'row',
+                        marginHorizontal: 8,
+                        paddingVertical: 18,
+                        borderTopWidth: 1,
+                        borderTopColor: '#FDF6ED',
+                    }}
+                >
+                    <Image
+                        source={images.user4}
+                        resizeMode="contain"
+                        style={{
+                            height: 52,
+                            width: 52,
+                            borderRadius: 26,
+                        }}
+                    />
+
+                    <View
+                        style={{
+                            flex: 1,
+                            height: 52,
+                            borderRadius: 26,
+                            borderWidth: 1,
+                            borderColor: '#CCC',
+                            marginLeft: 12,
+                            paddingLeft: 12,
+                            justifyContent: 'center',
+                        }}
+                    >
+                        <TextInput
+                            placeholder="Add comentário"
+                            placeholderTextColor="#CCC"
+                        />
+                    </View>
+                </View>
+            </View>
+        )
+    }
+
+
+
+    return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: '#E7E7E7' }}>
+            <View style={{ flex: 1, paddingHorizontal: 22 }}>
+              <ScrollView>
+                {renderFeedPost()}
+                {renderFeedPost() }
+                {renderFeedPost()}
+              </ScrollView>
+                
+            </View>
+        </SafeAreaView>
+    )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+export default Feed
+
+
+
+
+
+// // import { StyleSheet } from 'react-native';
+
+// // import EditScreenInfo from '../../components/EditScreenInfo';
+// // import { Text, View } from '../../components/Themed';
+
+// // export default function TabOneScreen() {
+// //   return (
+// //     <View style={styles.container}>
+// //       <Text style={styles.title}>Tab One</Text>
+// //       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+// //       <EditScreenInfo path="app/(tabs)/index.tsx" />
+// //     </View>
+// //   );
+// // }
+
+// // const styles = StyleSheet.create({
+// //   container: {
+// //     flex: 1,
+// //     alignItems: 'center',
+// //     justifyContent: 'center',
+// //   },
+// //   title: {
+// //     fontSize: 20,
+// //     fontWeight: 'bold',
+// //   },
+// //   separator: {
+// //     marginVertical: 30,
+// //     height: 1,
+// //     width: '80%',
+// //   },
+// // });
