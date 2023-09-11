1fab32d515bbf8c5fb882f44f8e22bd2b00f5e86
Lucas <lucasvanlume@hotmail.com>
2023-09-11 16:24:42 -0300

    feat(community): Adding firebase instances and refactoring the post in the feed into a new component

--- services/firebaseConfig_examplo.ts
+++ services/firebaseConfig_examplo.ts
@@ -1,1 +1,33 @@
+
+
 // add configurações do Firebase SDK e mudar o nome do arquivo para 'firebaseConfig.ts'
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "XXX",
+  authDomain: "XXX",
+  projectId: "XXX",
+  storageBucket: "XXX",
+  messagingSenderId: "XXX",
+  appId: "XXX",
+  measurementId: "XXX"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
+
+// Initialize auth
+const auth = getAuth(app);
+// Initialize firestore
+const db = getFirestore(app);
+// Initialize auth
+const storage = getStorage(app);
+
+export { app, auth, db, storage };
