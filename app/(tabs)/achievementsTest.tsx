1999614db1a8d3adf28a4c30ba44f64bac6a058f
Bonna Borsoi <bonnagilborsoi@gmail.com>
2023-09-19 23:58:31 -0300

    add(AchievementsTest): added test buttons

--- app/(tabs)/achievementsTest.tsx
+++ app/(tabs)/achievementsTest.tsx
@@ -1,26 +1,52 @@
+// import { Button } from 'native-base';
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Alert, Button, StyleSheet,  TouchableOpacity } from 'react-native';
+
 
 const AchievementsTestScreen = () => {
-    // function renderHeader() {
-    //     return (
-    //         <View
-    //             style={{
-    //                 position: 'absolute',
-    //                 top: 0,
-    //                 left: 0,
-    //                 right: 0,
-    //                 backgroundColor: '#4B0082',
-    //                 paddingVertical: 145,
-    //                 paddingHorizontal: 0,
-    //                 borderBottomLeftRadius: 20,
-    //                 borderBottomRightRadius: 20,
-    //                 zIndex: -1,
-    //             }}
-    //         >
-    //         </View>
-    //     )
-    // }
+    function renderHeader() {
+        return (
+            <View
+                style={{
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    backgroundColor: '#4B0082',
+                    paddingVertical: 20,
+                    paddingHorizontal: 0,
+                    // borderBottomLeftRadius: 20,
+                    // borderBottomRightRadius: 20,
+                    zIndex: -1,
+                }}
+            >
+
+            {/* Button aqui*/}
+            <View style={styles.fixToText}>
+            <Button 
+            title="Voltar"
+            />
+
+            {/* TouchableOpacity */}
+            <TouchableOpacity style={styles.button} >
+                    <Text>Press Here</Text>
+            </TouchableOpacity>
+
+        {/* TouchableOpacity com imagem - testar*/}
+        {/* <TouchableOpacity style={styles.button}>
+          <Image
+            source={require('./caminho/para/sua/imagem.png')}
+            style={{ width: 30, height: 30 }} // Defina o tamanho da imagem conforme necessário
+          />
+          <Text>Pressione Aqui</Text>
+        </TouchableOpacity> */}
+
+        </View>
+
+            </View>
+
+        )
+    }
 
     
     return (
@@ -28,11 +54,32 @@ const AchievementsTestScreen = () => {
             
             <View style={{ flex: 1, paddingBottom: 60, backgroundColor: '#E7E7E7' }}>
 
-                    {/* { renderHeader() } */}
+                    { renderHeader() }
 
                 
             </View>
     )
 };
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      marginHorizontal: 16,
+    },
+    title: {
+      textAlign: 'center',
+      marginVertical: 8,
+    },
+    fixToText: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+    },
+    button: {
+        alignItems: 'center',
+        backgroundColor: '#DDDDDD',
+        padding: 10,
+      },
+
+  });
 
 export default AchievementsTestScreen;
