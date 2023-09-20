f0d74e10a4a9907ea2a659c6cc452466fdc211eb
Bonna Borsoi <bonnagilborsoi@gmail.com>
2023-09-19 22:50:26 -0300

    feat(achievements): add achievements page

--- app/(tabs)/achievementsTest.tsx
+++ app/(tabs)/achievementsTest.tsx
@@ -2,7 +2,37 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 const AchievementsTestScreen = () => {
+    // function renderHeader() {
+    //     return (
+    //         <View
+    //             style={{
+    //                 position: 'absolute',
+    //                 top: 0,
+    //                 left: 0,
+    //                 right: 0,
+    //                 backgroundColor: '#4B0082',
+    //                 paddingVertical: 145,
+    //                 paddingHorizontal: 0,
+    //                 borderBottomLeftRadius: 20,
+    //                 borderBottomRightRadius: 20,
+    //                 zIndex: -1,
+    //             }}
+    //         >
+    //         </View>
+    //     )
+    // }
 
+    
+    return (
+        
+            
+            <View style={{ flex: 1, paddingBottom: 60, backgroundColor: '#E7E7E7' }}>
+
+                    {/* { renderHeader() } */}
+
+                
+            </View>
+    )
 };
 
 export default AchievementsTestScreen;
