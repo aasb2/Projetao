494cf4a1dc6aa6fe20e0970f3dffc779278834c1
Severino <severinocsilva3@gmail.com>
2023-09-23 16:31:36 -0300

    feat(login): adding login

--- app/index.tsx
+++ app/index.tsx
@@ -1,43 +1,68 @@
-import * as React from "react";
-import { Login } from "./login/Login";
-import "react-native-gesture-handler";
+import React, { useEffect } from "react";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { User, onAuthStateChanged, GoogleAuthProvider, signInWithCredential } from 'firebase/auth';
+import { auth } from '../services/firebaseConfig';
+import { createNewUserDocument } from '../services/functions/login/loginUser';
 import * as WebBrowser from 'expo-web-browser';
+import * as Google from "expo-auth-session/providers/google"
+import { Login } from "./Login/Login";
+import { useNavigation } from 'expo-router'
+import Feed from "./(tabs)/feed";
 
-WebBrowser.maybeCompleteAuthSession();
 
 export default function Index() {
-    // const [userInfo, setUserInfo] = React.useState();
-    // const [request, response, promptAsync] = Google.useAuthRequest({
-    //     androidClientId: "100988905326-mslpnvg22mbk52vuee73q9updl5k8vm0.apps.googleusercontent.com",
-    //     webClientId: "100988905326-q1b6e6imq9ei9ae76via0fk9h862f83u.apps.googleusercontent.com"
-    // })
+    WebBrowser.maybeCompleteAuthSession();
+    const navigation = useNavigation();
 
-    // React.useEffect(() => {
-    //     if (response?.type == "success") {
-    //         const { id_token } = response.params;
-    //         const credential = GoogleAuthProvider.credential(id_token);
-    //         signInWithCredential(auth, credential);
-    //         signInWithPopup(auth, credential)
-    //             .then((result) => {
-    //                 console.log(result);
-    //             })
-    //             .catch((error) => {
-    //                 console.log(error);
-    //             });
-    //     }
-    // }, []);
 
-    // React.useEffect(() => {
-    //     const unsub = onAuthStateChanged(auth, async (user) => {
-    //         if (user) {
-    //             console.log(JSON.stringify(user, null, 2));
-    //             setUserInfo(user);
-    //         } else {
-    //             console.log("else");
-    //         }
-    //     });
-    //     return () => unsub();
-    // }, []);
+    const [userInfo, setUserInfo] = React.useState<User | null>();
+    const [request, response, promptAsync] = Google.useAuthRequest({
+        androidClientId: "100988905326-mslpnvg22mbk52vuee73q9updl5k8vm0.apps.googleusercontent.com",
+        webClientId: "100988905326-q1b6e6imq9ei9ae76via0fk9h862f83u.apps.googleusercontent.com"
+    })
 
-    return <Login />;
+    const checkLocalUser = async () => {
+        try {
+            const userJSON = await AsyncStorage.getItem("@user");
+            const userData = userJSON ? JSON.parse(userJSON) : null;
+            setUserInfo(userData);
+        } catch (error) {
+            alert(error);
+        }
+    }
+
+    useEffect(() => {
+        checkLocalUser();
+        const unsub = onAuthStateChanged(auth, async (user) => {
+            if (user) {
+                console.log(JSON.stringify(user, null, 2));
+                await AsyncStorage.setItem("@user", JSON.stringify(user));
+                setUserInfo(user);
+                navigation.navigate('(tabs)')
+            } else {
+                setUserInfo(null);
+            }
+        });
+        return () => unsub();
+    }, []);
+
+    useEffect(() => {
+        if (response?.type == "success") {
+            const { id_token } = response.params;
+            const credential = GoogleAuthProvider.credential(id_token);
+            signInWithCredential(auth, credential)
+            .then((result) => {
+                
+                createNewUserDocument(result.user);
+                setUserInfo(result.user);
+                // TODO:                
+                // jogar p tela de colocar id do personal
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        }
+    }, [response]);
+
+    return <Login promptAsync={promptAsync} />;
 }
--- app/login/Login.tsx
+++ app/login/Login.tsx
@@ -1,75 +1,89 @@
-import { useState } from 'react';
+import React from 'react';
 import appTheme from '../../constants/theme';
-import { View, Text, Image, TouchableOpacity } from 'react-native';
-import { Link } from 'expo-router';
+import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
-import { auth } from "../../services/firebaseConfig";
-import {
-    GoogleAuthProvider,
-    User,
-    signInWithPopup,
-} from "firebase/auth"
 
 
-export function Login() {
-  const [user, setUser] = useState<User>({} as User);
-
-  function handleGoogleSigIn() {
-        const provider = new GoogleAuthProvider();
-
-        signInWithPopup(auth, provider)
-            .then((result) => {
-                setUser(result.user);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }
-
-  return (
-    <View
-      style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: appTheme.COLORS.primary}}
-    >
-      <Image
-        source={require('../../assets/images/logo.png')}
-        style={{ width: 120, height: 120 }}
-      />
-      <Text style={{ paddingTop: 10, fontSize: 32, color: appTheme.COLORS.white, fontWeight: 'bold', fontFamily: 'Roboto_700Bold'}}>
-          Unity FIT
-      </Text>
-
-      <Text style={{ marginTop: 35, fontSize: 16, color: appTheme.COLORS.secondary, fontWeight: 'bold', fontFamily: 'Roboto_400Regular'}}>
-        Acesse com:
-      </Text>
-
-      <View style={{marginTop: 20}}>
-        <View 
-          style={{flex:1, flexDirection: 'row', gap: 10, justifyContent: 'center', alignItems: 'center'}}
+export function Login({ promptAsync }: any) {
+    return (
+        <View
+            style={styles.container}
         >
-          <TouchableOpacity
-              onPress={() => handleGoogleSigIn()} // Chame uma função quando o botão for pressionado
-              style={{
-                width: 36,
-                height: 36,
-              }}
-          >
-              <AntDesign
-                  name="google"
-                  size={30}
-                  color="white" // Altere a cor do ícone com base no estado de "isLiked" do post
-              />
-          </TouchableOpacity>
-          
-          
-          <Link href={'/'}>
-            <Image 
-              source={require('../../assets/images/facebook.png')}
-              style={{ width: 36, height: 36 }}
-            />
-          </Link>
+            <View
+                style={styles.containerLogo}
+            >
+                <Image
+                    source={require('../../assets/images/logo.png')}
+                    style={{ width: 120, height: 120, marginTop: 100 }}
+                />
+
+                <Text
+                    style={styles.title}
+                >
+                    Unity FIT
+                </Text>
+            </View>
+
+
+            <View style={styles.containerForm}>
+                <Text style={styles.description}>
+                    Acesse com:
+                </Text>
+
+                <View style={styles.icons}>
+                    <TouchableOpacity
+                        onPress={() => promptAsync()}
+                        style={{
+                            width: 36,
+                            height: 36,
+                        }}
+                    >
+                        <AntDesign
+                            name="google"
+                            size={30}
+                            color="white"
+                        />
+                    </TouchableOpacity>
+                </View>
+            </View>
         </View>
-      </View>
-      
-    </View>
-  );
+    );
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: appTheme.COLORS.primary,
+        paddingTop: 100
+    },
+    containerLogo: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        alignContent: 'center',
+    },
+    containerForm: {
+        flex: 1,
+        alignItems: 'center',
+        paddingTop: 30
+    },
+    title: {
+        marginTop: 10,
+        fontSize: 35,
+        color: 'white',
+        fontFamily: 'Roboto_700Bold'
+    },
+    description: {
+        fontFamily: 'Roboto_400Regular',
+        color: appTheme.COLORS.secondary,
+        fontSize: 16,
+    },
+    icons: {
+        paddingTop: 10,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+});
