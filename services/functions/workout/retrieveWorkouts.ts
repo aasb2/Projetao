899505277888a73cc2e3773cb052ecfda5fd1776
joaomarcelovalencachacon <joaomarcelovalencachacon@gmail.com>
2023-09-28 15:17:42 -0300

    refactoring(retrieveWorkouts): changed names for better understanding

---
+++ services/functions/workout/retrieveWorkouts.ts
@@ -0,0 +1,44 @@
+import { db } from '../../firebaseConfig';
+import { collection, getDoc, query, where, getDocs,doc} from 'firebase/firestore';
+
+async function retrieveWorkouts(userId: string) {
+  try {
+    const userDocRef = doc(db, 'users', userId);
+    const userDocSnapshot = await getDoc(userDocRef);
+
+    if (userDocSnapshot.exists()) {
+      const userDocData = userDocSnapshot.data();
+      const workoutReferences = userDocData.workouts || [];
+
+      const workoutsData = [];
+
+      for (const workoutRef of workoutReferences) {
+        const workoutDocSnapshot = await getDoc(workoutRef);
+        if (workoutDocSnapshot.exists()) {
+          const workoutData = workoutDocSnapshot.data();
+
+          // Nome do treino
+          const nomeTreino = workoutData.treino;
+          workoutData.nomeTreino = nomeTreino;
+
+          // Exercícios do treino
+          const exercises = workoutData.exercises;
+          workoutData.exercises = exercises;
+
+          workoutsData.push(workoutData);
+        }
+      }
+
+      console.log("aqui");
+      return workoutsData;
+    } else {
+      console.error('Documento de usuário não encontrado');
+      return [];
+    }
+  } catch (error) {
+    console.error('Erro ao buscar a lista de treinos:', error);
+    throw error;
+  }
+}
+
+export { retrieveWorkouts };
