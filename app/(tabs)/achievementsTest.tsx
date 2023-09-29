635b75490f7adb4981beea1f3df3c301a305c06d
victorluizz <victorluizlima1@gmail.com>
2023-09-28 21:26:39 -0300

    Estilizando a tela de conquistas.

--- app/(tabs)/achievementsTest.tsx
+++ app/(tabs)/achievementsTest.tsx
@@ -1,86 +1,125 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, FlatList, TextInput, Dimensions } from 'react-native';
-import { Ionicons } from '@expo/vector-icons'; // Importe o ícone desejado da biblioteca de ícones
-import { getAchievementsList } from '../../services/functions/achievements/functionAchievements'; // Substitua pelo caminho correto do seu arquivo getAchievementsList
-import appTheme from '../../constants/theme';
-import { background } from 'native-base/lib/typescript/theme/styled-system';
-import { Button } from 'react-native';
-import { ChallengeCard } from '../../components/challengeCard';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  FlatList,
+  TextInput,
+  Dimensions,
+  Image,
+} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { getAchievementsList } from '../../services/functions/achievements/functionAchievements';
 
+const { width } = Dimensions.get('window');
 
-// Defina o tipo de objeto para os achievements
+const maxRectangleWidth = width * 0.45;
+const horizontalSpacing = 10;
+
 type Achievement = {
   id: string;
   achievementName: string;
   description: string;
-  // Outras propriedades, se necessário
 };
 
 const AchievementsTestScreen = () => {
   const [achievements, setAchievements] = useState<Achievement[]>([]);
   const [searchText, setSearchText] = useState('');
   const [filteredAchievements, setFilteredAchievements] = useState<Achievement[]>([]);
-
   const [showAchievements, setShowAchievements] = useState(true);
   const [showChallenges, setShowChallenges] = useState(false);
 
-
   useEffect(() => {
-    // Função assíncrona para buscar a lista de achievements
-
     async function fetchAchievements() {
       try {
         const achievementsData = await getAchievementsList();
         setAchievements(achievementsData);
       } catch (error) {
         console.error('Erro ao buscar a lista de achievements:', error);
-        // Trate o erro de acordo com a necessidade
       }
     }
 
-  // Chama a função para buscar os achievements
-  fetchAchievements();
-}, []);
+    fetchAchievements();
+  }, []);
 
   useEffect(() => {
-
-    // Filtra os achievements com base no texto de pesquisa
     const filtered = achievements.filter((achievement) =>
       achievement.achievementName.toLowerCase().includes(searchText.toLowerCase())
     );
     setFilteredAchievements(filtered);
   }, [searchText, achievements]);
 
-  // Componente: achievement
-  function renderItem({item }: { item: Achievement }) {
+  function renderItem({ item }: { item: Achievement }) {
     return (
       <View style={styles.achievementItem}>
-        {/* <Image source={{ uri: item.imageURL }} style={styles.achievementImage} /> */}
-        <Text>{item.achievementName}</Text>
+        <Text style={styles.achievementName}>{item.achievementName}</Text>
         <Text style={styles.achievementDescription}>{item.description}</Text>
       </View>
     );
   }
 
+  const rectangleContents = [
+    {
+      topText: 'Mora na academia',
+      bottomText: '100% de frequência durante 6 meses',
+      imageURL: 'URL_DA_SUA_IMAGEM_1',
+    },
+    {
+      topText: 'Ombro de aço',
+      bottomText: '50kg no desenvolvimento com barra',
+      imageURL: 'URL_DA_SUA_IMAGEM_2',
+    },
+    {
+      topText: 'Bíceps real natty',
+      bottomText: '10kg na rosca de bíceps',
+      imageURL: 'URL_DA_SUA_IMAGEM_3',
+    },
+    {
+      topText: 'Projeto de Monstro',
+      bottomText: '40 treinos completados',
+      imageURL: 'URL_DA_SUA_IMAGEM_4',
+    },
+    {
+      topText: 'Bíceps do Popeye',
+      bottomText: '15kg na rosca de bíceps ',
+      imageURL: 'URL_DA_SUA_IMAGEM_5',
+    },
+    {
+      topText: 'Mestre do agachamento',
+      bottomText: '300 séries de agachamento realizados',
+      imageURL: 'URL_DA_SUA_IMAGEM_6',
+    },
+  ];
+
+  const rectangles = rectangleContents.map((content, index) => (
+    <View style={[styles.rectangle, { maxWidth: maxRectangleWidth }]} key={index}>
+      <Text style={styles.topText}>{content.topText}</Text>
+      <Image
+        source={{ uri: content.imageURL }}
+        style={styles.image}
+        resizeMode="cover"
+      />
+      <Text style={styles.bottomText}>{content.bottomText}</Text>
+    </View>
+  ));
+
   return (
     <View style={{ flex: 1, paddingBottom: 60, backgroundColor: '#380062' }}>
-
-      {/* Header */}
       <View style={styles.header}>
-        {/* Botão voltar */}
         <TouchableOpacity
           style={{
             flexDirection: 'row',
             alignItems: 'center',
-            backgroundColor: '4B0082',
+            backgroundColor: '#4B0082',
             padding: 10,
-          }}>
+          }}
+        >
           <Ionicons name="arrow-back" size={24} color="white" />
           <Text style={{ color: 'white', marginLeft: 5 }}>Voltar</Text>
         </TouchableOpacity>
       </View>
 
-      {/* Botões para selecionar entre achievements ou challenges */}
       <View style={styles.buttonSelectionContainer}>
         <TouchableOpacity
           style={showAchievements ? styles.selectedButton : styles.unselectedButton}
@@ -102,7 +141,6 @@ const AchievementsTestScreen = () => {
         </TouchableOpacity>
       </View>
 
-      {/* Barra de pesquisa */}
       <View style={styles.searchContainer}>
         <View style={styles.searchInputContainer}>
           <TextInput
@@ -112,36 +150,35 @@ const AchievementsTestScreen = () => {
             onChangeText={(text) => setSearchText(text)}
             onSubmitEditing={() => {}}
           />
-          <TouchableOpacity
-            style={styles.searchButton}
-            onPress={() => {}}
-          >
+          <TouchableOpacity style={styles.searchButton} onPress={() => {}}>
             <Ionicons name="search" size={24} color="black" />
           </TouchableOpacity>
         </View>
       </View>
 
-      {/* RENDERIZAÇÃO: Achievements */}
-      {/* @victor */}
       {showAchievements && (
-      <FlatList
-        data={filteredAchievements}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id}
-      />
+        <FlatList
+          data={filteredAchievements}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+        />
       )}
 
-      {/* RENDERIZAÇÃO: Challenges */}
-      {/* @bonna */}
-      <ChallengeCard />
-
-
+      {showAchievements && (
+        <FlatList
+          data={rectangles}
+          numColumns={2}
+          renderItem={({ item }) => (
+            <View style={styles.rectangleContainer}>{item}</View>
+          )}
+          keyExtractor={(_, index) => index.toString()}
+          style={{ marginTop: 20 }}
+        />
+      )}
     </View>
   );
 };
 
-
-// Estilização
 const styles = StyleSheet.create({
   header: {
     flexDirection: 'row',
@@ -150,29 +187,31 @@ const styles = StyleSheet.create({
     backgroundColor: '#4B0082',
     padding: 10,
   },
-  button: {
-    alignItems: 'center',
-    backgroundColor: '#DDDDDD',
-    padding: 10,
-  },
   achievementItem: {
-    flexDirection: 'row',
+    flexDirection: 'column',
     alignItems: 'center',
     padding: 16,
     borderBottomWidth: 1,
     borderBottomColor: '#DDDDDD',
-    width: 200,
-    height: 200,
+    width: maxRectangleWidth,
     backgroundColor: '#4B0082',
   },
-  achievementDescription: {
+  achievementName: {
     fontSize: 16,
-    color: 'black',
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 5, // Adicionado espaço entre o nome e a descrição
+  },
+  achievementDescription: {
+    fontSize: 14,
+    color: 'white',
+    textAlign: 'center',
   },
   searchContainer: {
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop: 20, // Espaçamento entre a barra de pesquisa e a header
+    marginTop: 20,
   },
   searchInputContainer: {
     flexDirection: 'row',
@@ -195,28 +234,56 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop: 10, 
-  
+    marginTop: 10,
   },
   selectedButton: {
-    backgroundColor: '#B397C8', // Cor de fundo quando o botão está selecionado
-    borderRadius: 5, // Borda arredondada
-    padding: 10, // Preenchimento interno
-    marginHorizontal: 5, // Espaçamento horizontal entre botões
+    backgroundColor: '#B397C8',
+    borderRadius: 5,
+    padding: 10,
+    marginHorizontal: 5,
   },
   unselectedButton: {
-    backgroundColor: '#4B0082', // Cor de fundo quando o botão não está selecionado
-    borderRadius: 5, // Borda arredondada
-    padding: 10, // Preenchimento interno
-    marginHorizontal: 5, // Espaçamento horizontal entre botões
+    backgroundColor: '#4B0082',
+    borderRadius: 5,
+    padding: 10,
+    marginHorizontal: 5,
   },
   buttonText: {
-    color: 'white', // Cor do texto
-    fontSize: 16,   // Tamanho da fonte do texto
-    fontWeight: 'bold', // Peso da fonte (pode ser ajustado conforme necessário)
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
-  
-
+  rectangleContainer: {
+    flex: 1,
+    paddingHorizontal: horizontalSpacing / 2,
+    marginBottom: 20,
+  },
+  rectangle: {
+    width: '100%',
+    maxWidth: maxRectangleWidth,
+    aspectRatio: 153 / 198,
+    backgroundColor: '#4B0082',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 10,
+    alignSelf: 'center',
+  },
+  topText: {
+    color: 'white',
+    fontSize: 16,
+    marginTop: 10,
+    textAlign: 'center',
+  },
+  bottomText: {
+    color: 'white',
+    fontSize: 14, // Ajustado o tamanho da fonte para evitar que os textos ultrapassem
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+  image: {
+    width: '100%',
+    height: '45%', // Ajustado a altura da imagem
+  },
 });
 
 export default AchievementsTestScreen;
