f9d6d40e14c73e96319828f27042c397d13b54a2
joaomarcelovalencachacon <joaomarcelovalencachacon@gmail.com>
2023-09-30 04:39:12 -0300

    feat(checkConditions): now checkConditions works for all challenges

---
+++ services/functions/achievements/checkCondition.ts
@@ -0,0 +1,63 @@
+import { db, auth } from '../../firebaseConfig';
+import { doc, getDoc, updateDoc, collection, getDocs } from 'firebase/firestore';
+
+async function checkCondition(userID: string) {
+  try {
+    // Consulta para obter o documento do usuário
+    const userDocRef = doc(db, 'users', userID);
+    const userDocSnapshot = await getDoc(userDocRef);
+    
+    if (!userDocSnapshot.exists()) {
+      throw new Error(`Usuário com ID ${userID} não encontrado.`);
+    }
+
+    // Consulta para obter o documento do desafio
+    const challengesCollection = collection(db, 'challenges');
+    const challengesQuerySnapshot = await getDocs(challengesCollection);
+
+    const userData = userDocSnapshot.data();
+    for (const challengeDoc of challengesQuerySnapshot.docs){
+        const challengeData = challengeDoc.data();
+        console.log(challengeData);
+        
+        // Obtenha as condições do desafio
+        const conditions = challengeData.conditions || {};
+        // Verifique as condições
+        const challengeKey = conditions.key;
+        const challengeValue = conditions.value;
+        console.log(userData.maximum_weights[challengeKey]);
+        console.log(challengeValue);
+
+
+        if (userData.maximum_weights[challengeKey] >= challengeValue) {
+            console.log(challengeData.name)
+            
+            // Atualize o valor do desafio para true
+            const userChallengesRef = doc(db, 'users', userID);
+            await updateDoc(userChallengesRef, {
+              [`userChallenges.${challengeDoc.id}`]: true,
+            });
+          } else {
+            // Atualize o valor do desafio para false
+            const userChallengesRef = doc(db, 'users', userID);
+            await updateDoc(userChallengesRef, {
+              [`userChallenges.${challengeDoc.id}`]: false,
+            });
+          }
+    }
+    
+    return true;
+    
+  } catch (error) {
+    console.error('Erro ao verificar condição:', error);
+    throw error;
+  }
+}
+
+export { checkCondition };
+
+// maximumWeights[exerciseName] = exerciseWeight;
+
+  
+// // Atualize o campo "maximum_weights" no documento do usuário
+// await updateDoc(userDocRef, { maximum_weights: maximumWeights });
